007d5d13de725de458f846de5dc4f3f615d86b79
lisaped <44180795+lisaped@users.noreply.github.com>
2026-03-22 12:29:10 +1100

    Add new string constants for datepicker and settings

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -25,10 +25,28 @@ export const SUCCESS_RATE_TEXT = 'Success rate'
 export const CURRENT_STREAK_TEXT = 'Current streak'
 export const BEST_STREAK_TEXT = 'Best streak'
 
-// Required by App.tsx
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
   'To get the best experience, please open this game directly in your browser rather than within an app.'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard mode can only be enabled at the start of a new game!'
 export const SHARE_FAILURE_TEXT =
   'Unable to share the results of the game. Please try again.'
+
+export const DATEPICKER_TITLE = 'Choose a past date'
+export const DATEPICKER_CHOOSE_TEXT = 'Choose'
+export const DATEPICKER_TODAY_TEXT = 'today'
+
+export const ARCHIVE_GAMEDATE_TEXT = 'Archive game date'
+export const SETTINGS_TITLE = 'Settings'
+export const HARD_MODE_DESCRIPTION =
+  'Any revealed hints must be used in subsequent guesses'
+export const DARK_MODE_DESCRIPTION = 'For those who prefer darker themes'
+export const HIGH_CONTRAST_MODE_DESCRIPTION =
+  'For improved colour vision accessibility'
+export const MIGRATE_BUTTON_TEXT = 'Transfer your stats'
+export const MIGRATE_DESCRIPTION_TEXT =
+  'Click here to transfer your statistics to a new device.'
+export const NOT_CONTAINED_MESSAGE = (letter: string) =>
+  `Guess must contain ${letter}`
+export const WRONG_SPOT_MESSAGE = (letter: string, position: number) =>
+  `Must use ${letter} in position ${position}`
