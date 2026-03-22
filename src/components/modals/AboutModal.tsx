baec490e0916ec7b32f93f636820b452f03530ec
lisaped <44180795+lisaped@users.noreply.github.com>
2026-03-22 11:58:18 +1100

    Create AboutModal.tsx

---
+++ src/components/modals/AboutModal.tsx
@@ -0,0 +1,21 @@
+<p className="text-sm text-gray-500 dark:text-gray-300">
+  A word game inspired by the fiction novel{' '}
+  
+    href="https://www.lisapedrosa.com/speciest"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="underline font-bold"
+  >
+    Speciest by Lisa Pedrosa
+  </a>
+  . Visit{' '}
+  
+    href="https://lisapedrosa.art"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="underline"
+  >
+    lisapedrosa.art
+  </a>{' '}
+  to explore more of Lisa's work.
+</p>
